c11e628665b933e62b4d52fb916429a175a5a183
Vitalii <ilovepinkpony1@gmail.com>
2019-06-22 14:46:17 +0300

    Updates

---
+++ precache-manifest.14eb416f24ee0c1ba8bc1610ea7ed3b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e1ad7896d3b223c9dd525150b69539",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "29a3cf3053cea812531e",
+    "url": "/react-app/static/css/main.8b1b8d3f.chunk.css"
+  },
+  {
+    "revision": "905979b9c8221108fa62",
+    "url": "/react-app/static/js/2.bfeffea3.chunk.js"
+  },
+  {
+    "revision": "29a3cf3053cea812531e",
+    "url": "/react-app/static/js/main.e2804a09.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  }
+]);
